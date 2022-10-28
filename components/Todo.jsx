987d3f1ea438c6ec5f7d72057b2c90d26c58e245
adriansth <soto.herrera.adrian@gmail.com>
2022-10-28 14:58:08 -0500

    feat: Initialize server with Express.

---
+++ components/Todo.jsx
@@ -0,0 +1,10 @@
+export default function Todo() {
+    return(
+        <div>
+            <h2>Todo 1</h2>
+            <p>Description 1</p>
+            <h2>Todo 2</h2>
+            <p>Description 2</p>
+        </div>
+    );
+}

9db341bfb3c659d2630a472797f4044757d0e98d
adriansth <soto.herrera.adrian@gmail.com>
2022-10-28 21:07:32 -0500

    feat: Implement GET method at client side.

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,10 +1,27 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export default function Todo() {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/tasks')
+            .then((res) => setTasks(res.data.data));
+    }, []);
+
     return(
         <div>
-            <h2>Todo 1</h2>
-            <p>Description 1</p>
-            <h2>Todo 2</h2>
-            <p>Description 2</p>
+            <h2 className='text-5xl font-bold'>Tasks</h2>
+            {
+                tasks.map((task) => (
+                    <div key={task.id}>
+                        <h3 className='font-bold text-xl'>{task.title}</h3>
+                        <p className='text-lg'>{task.description}</p>
+                        <p className='text-xs'>{task.createdAt}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }

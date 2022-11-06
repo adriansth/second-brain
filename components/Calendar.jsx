52b27558bb5f68647d8eb5d92bee10faeeef73f5
adriansth <soto.herrera.adrian@gmail.com>
2022-11-05 22:56:46 -0600

    feat: Migrated calendar state to redux global state.

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -1,14 +1,18 @@
-import { useState, useEffect, useContext } from 'react';
+//redux
+import { useSelector } from 'react-redux';
+// hooks 
+import { useState } from 'react';
+// utils
+import { getMonth } from '../utils/dates';
 // components
 import CalendarHeader from '../components/CalendarHeader';
 import CalendarMonth from '../components/CalendarMonth';
 import CalendarSidebar from '../components/CalendarSidebar';
-// utils
-import { getMonth } from '../utils/dates';
 
 export default function Calendar() {
 
-    const [currentMonth, setCurrentMonth] = useState(getMonth());
+    const {calendarMonthIndex} = useSelector(state => state.calendarMonthIndex); // bring the calendar index state
+    const [currentMonth, setCurrentMonth] = useState(getMonth(calendarMonthIndex)); // get the month matrix depending on global calendar index state
 
     return(
         <div className='w-[80%] lg:w-[85%] h-[92%]'>
--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,37 @@
+// utils 
+import dayjs from 'dayjs';
+
 const INITIAL_STATE = {
-
+    calendarMonthIndex: dayjs().month(), // month num from 0 to 11 
 }
 
-const reducer = (state = INITIAL_STATE, action) => {
-
+const calendarReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        // this action increments the month index by 1
+        case "INCREMENT_MONTH":
+            return {
+                ...state,
+                calendarMonthIndex: state.calendarMonthIndex + 1,
+            };
+        // this action decrements the month index by 1
+        case "DECREMENT_MONTH":
+            return {
+                ...state,
+                calendarMonthIndex: state.calendarMonthIndex - 1,
+            };
+        // this action resets the month index to today's 
+        case "RESET_MONTH":
+            return {
+                ...state,
+                calendarMonthIndex: dayjs().month(),
+            };
+        // the default value is today's month index
+        default:
+            return {
+                ...state,
+                calendarMonthIndex: dayjs().month(),
+            };
+    }
 }
 
-export default reducer;
+export default calendarReducer;

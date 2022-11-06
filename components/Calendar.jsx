eaf60feb9592238fa9b77dd67ae091c584d33451
adriansth <soto.herrera.adrian@gmail.com>
2022-11-05 23:30:10 -0600

    feat: Working calendar month explorer with redux.

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -8,18 +8,18 @@ import { getMonth } from '../utils/dates';
 import CalendarHeader from '../components/CalendarHeader';
 import CalendarMonth from '../components/CalendarMonth';
 import CalendarSidebar from '../components/CalendarSidebar';
+import { useEffect } from 'react';
 
 export default function Calendar() {
 
-    const {calendarMonthIndex} = useSelector(state => state.calendarMonthIndex); // bring the calendar index state
-    const [currentMonth, setCurrentMonth] = useState(getMonth(calendarMonthIndex)); // get the month matrix depending on global calendar index state
+    const calendarMonthIndex = useSelector(state => state.calendarMonthIndex); // bring the calendar index state
 
     return(
         <div className='w-[80%] lg:w-[85%] h-[92%]'>
             <div className='w-full h-full'>
                 <CalendarHeader />
                 <div className='w-full h-full flex items-center justify-center'>
-                    <CalendarMonth month={currentMonth} />
+                    <CalendarMonth month={getMonth(calendarMonthIndex)} />
                 </div>
             </div>
         </div>
--- components/CalendarHeader.jsx
+++ components/CalendarHeader.jsx
@@ -1,17 +1,43 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+// redux 
+import { useDispatch, useSelector } from "react-redux";
 
 export default function CalendarHeader() {
 
+    const dispatch = useDispatch(); // alias
+
+    // increment month index dispatch 
+    const incrementMonthIndex = () => {
+        dispatch({type: "INCREMENT_MONTH"}); 
+    }
+
+    // decrement month index dispatch
+    const decrementMonthIndex = () => {
+        dispatch({type: "DECREMENT_MONTH"});
+    }
+
+    // reset month index dispatch
+    const resetMonthIndex = () => {
+        dispatch({type: "RESET_MONTH"});
+    }
+
     return(
         <div className='px-4 py-2 flex items-center gap-x-3'>
-            <button className='text-sm p-2 border hover:bg-blue-400 hover:text-white cursor-pointer rounded-lg'>Today</button>
             <button 
-                className='border p-2 rounded-lg hover:bg-blue-400 hover:text-white cursor-pointer'   
+                className='text-sm p-2 border hover:bg-blue-400 hover:text-white cursor-pointer rounded-lg'
+               onClick={resetMonthIndex} 
+            >
+                Today
+            </button>
+            <button 
+                className='border p-2 rounded-lg hover:bg-blue-400 hover:text-white cursor-pointer'
+                onClick={decrementMonthIndex}   
             >
                 <ChevronLeftIcon className='w-5' />
             </button>
             <button 
                 className='border p-2 rounded-lg hover:bg-blue-400 hover:text-white cursor-pointer'
+                onClick={incrementMonthIndex}
             >
                 <ChevronRightIcon className='w-5' />
             </button>

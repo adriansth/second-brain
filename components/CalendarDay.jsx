007e8f9a21b9996b0d9345bcb852e3f68c538df7
adriansth <soto.herrera.adrian@gmail.com>
2022-11-09 18:01:59 -0600

    styles: Adjusted styles for calendar.

--- components/CalendarDay.jsx
+++ components/CalendarDay.jsx
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs';
+// icons 
+import { PlusIcon } from '@heroicons/react/24/outline';
 
 export default function CalendarDay({ day, rowIdx }) {
 
     const getCurrentDayClass = () => {
        if (day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")) {
-           return 'bg-red-400 text-white rounded-full w-7 font-bold'
+           return 'bg-red-400 text-white rounded-full w-7'
        } else {
            return "";
        }
@@ -15,14 +17,19 @@ export default function CalendarDay({ day, rowIdx }) {
             <div className='flex flex-col items-center'>
                 {
                     rowIdx === 0 && 
-                    <p className='font-bold text-blue-400 border-b w-full text-center bg-gray-100'>{day.format('ddd').toUpperCase()}</p>                
+                    <p className='font-bold text-white border-b w-full text-center bg-blue-400'>{day.format('ddd').toUpperCase()}</p>                
                 }
                 <div className='w-full pr-3 flex justify-end'>
-                    <p className={`text-xs p-1 my-1 text-center ${getCurrentDayClass()}`}>
+                    <p className={`text-xs p-1 my-1 text-center font-bold ${getCurrentDayClass()}`}>
                         {day.format('D')}
                     </p>
                 </div>
-            </div>
+                <div className='flex justify-end w-full h-full'>
+                    <div className='w-7 p-1 mx-1'>
+                        <PlusIcon className='w-5 p-1 hover:bg-blue-400 hover:text-white cursor-pointer rounded-full' />
+                    </div>
+                </div>
+           </div>
         </div>
     );
 }
